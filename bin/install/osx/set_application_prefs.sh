6aecf2a8c2e949cf2ae85d448b2f8ec9ec52e0ed
Mark Smith <markjgsmith@gmail.com>
2016-01-03 11:51:01 +0700

    Added basic structue / outline

--- bin/install/osx/set_application_prefs.sh
+++ bin/install/osx/set_application_prefs.sh
@@ -1,1 +1,27 @@
 #!/bin/sh
+
+# /System/Library/CoreServices/Finder
+# /System/Library/CoreServices/HelpViewer
+# /System/Library/CoreServices/ReportCrash
+# /System/Library/CoreServices/SystemUIServer
+
+# Move into set_system_prefs: ?
+# System Preferences > Users & Groups > Login Items
+# System Preferences > Growl
+# System Preferences > Choosy
+
+# Applications: iTunes
+# Applications: Mail
+# Applications: Quicktime Player
+# Applications: Remote Destop
+# Applications: Safari
+# Applications: Skype
+# Applications: Total Finder
+# Applications: Transmission
+# Applications: VMWare Fusion
+# etc
+
+# Utilities: Audio Hijack Pro
+# Utilities: Disk Utility
+# Utilities: Dropbox
+# Utilities: Handbrake
--- bin/install/osx/set_system_prefs.sh
+++ bin/install/osx/set_system_prefs.sh
@@ -1,1 +1,39 @@
 #!/bin/sh
+
+# General
+# Desktop and screensaver
+# Dock
+# Mission control
+# Language and region
+# Security and privacy
+# Spotlight
+# Notifications
+
+# Universal access ?
+# CDs & DVDs ? (Sharing ?)
+
+# Displays
+# Energy saver
+# Keyboard
+# Mouse
+# Trackpad
+# Printers and scanners
+# Sound
+
+# iCloud
+# Internet accounts
+# Extensions
+# Network
+# Bluetooth
+# Sharing
+
+# Users and groups
+# Parental control
+# App store
+# Dictation and speech
+# Date & time
+# Startup disk
+# Softare update ? (App store - check for app store updates ?)
+# Speech ? (Dictation and speech ?)
+# Time machine
+# Accessibility
