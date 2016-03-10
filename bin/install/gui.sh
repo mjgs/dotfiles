9faf1f6d60f0b8fd97178a4ceadc6ac321120bb8
Mark Smith <markjgsmith@gmail.com>
2016-03-11 01:52:22 +0800

    Update brew cask check

--- bin/install/gui.sh
+++ bin/install/gui.sh
@@ -7,7 +7,7 @@ if [ ! -x /usr/local/bin/brew ]; then
   exit 1
 fi
 
-if [ ! -x /usr/local/opt/brew-cask ]; then
+if [ ! -x /usr/local/Library/Taps/caskroom/homebrew-cask/brew-cask.rb ]; then
   echo "ERROR: Homebrew Cask must be installed to run the gui.sh installer script"
   exit 1
 fi
