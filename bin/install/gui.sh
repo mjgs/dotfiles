52fbac88e69f63fb8715b14928293410cf45706a
Mark Smith <markjgsmith@gmail.com>
2016-06-21 13:38:45 +0700

    Remove unused gui packages

--- bin/install/gui.sh
+++ bin/install/gui.sh
@@ -14,7 +14,6 @@ fi
 
 echo "Installing brew cask applications..."
 brew cask install adobe-reader
-brew cask install adobe-photoshop-cc
 brew cask install alfred
 brew cask install calibre
 brew cask install cord
@@ -22,11 +21,8 @@ brew cask install cyberghost
 brew cask install dropbox
 brew cask install evernote
 brew cask install firefox
-#brew cask install get-iplayer-automator https://github.com/GetiPlayerAutomator/get-iplayer-automator
 brew cask install google-chrome
-#brew cask install gpodder
 brew cask install handbrake
-#brew cask install hemmingway-editor http://www.hemingwayapp.com
 brew cask install id3-editor
 brew cask install imageoptim
 brew cask install iterm2
@@ -37,19 +33,12 @@ brew cask install malwarebytes-anti-malware
 brew cask install mongohub
 brew cask install omnidisksweeper
 brew cask install opera
-brew cask install sequel-pro
 brew cask install shiftit
 brew cask install skype
-brew cask install slack
 brew cask install sublime-text
 brew cask install vlc
-#brew cask install wd-drive-utilities http://download.wdc.com/wdapp/WD_Drive_Utilities_Installer_2_0_2_18.zip
 brew cask install wd-security
 brew cask install webstorm
-brew cask install robomongo
-brew cask install dockertoolbox
-
-# TODO - create casks for get0iplayer-automator, hemmingway-editor, wd-drive-utilities
 
 if [ -x $CONFIGS_DIR/gui_local.sh ]; then
   $CONFIGS_DIR/gui_local.sh			
