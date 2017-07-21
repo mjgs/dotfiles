8c53f9bcfad373671785eb6afccbeae3d0fc9b13
Mark Smith <markjgsmith@gmail.com>
2017-07-21 16:54:56 +0700

    Add Application folder rsync to backup

--- bin/backup_laptop_all.sh
+++ bin/backup_laptop_all.sh
@@ -6,6 +6,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+rsync -avh --delete --progress $HOME/Applications/ $USB_DRIVE_DIR/$(basename $HOME)/Applications
 rsync -avh --delete --progress $HOME/Desktop/ $USB_DRIVE_DIR/$(basename $HOME)/Desktop
 rsync -avh --delete --progress $HOME/Documents/ $USB_DRIVE_DIR/$(basename $HOME)/Documents
 rsync -avh --delete --progress $HOME/Downloads/ $USB_DRIVE_DIR/$(basename $HOME)/Downloads
