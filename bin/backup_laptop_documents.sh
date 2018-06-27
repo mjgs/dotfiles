c34a75d29aef954fed9ebae08f71518755ac84f9
Mark Smith <markjgsmith@gmail.com>
2018-06-27 16:57:53 +0700

    Add dotfiles comment

--- bin/backup_laptop_documents.sh
+++ bin/backup_laptop_documents.sh
@@ -28,7 +28,7 @@ rsync -avh --delete --progress $HOME/Sites/ $USB_DRIVE_DIR/$(basename $HOME)/Sit
 rsync -avh --delete --progress /usr/local/ $USB_DRIVE_DIR/USBLOCALBACKUP/usr/local
 rsync -avh --delete --progress /usr/local/ $USB_DRIVE_DIR/USBLOCALBACKUP/usr/local
 rsync -avh --delete --progress $HOME/.* $USB_DRIVE_DIR/OLDHOMEDIRDOTFILES
-rsync -avh --delete --progress $HOME/.??* $USB_DRIVE_DIR/OLDHOMEDIRDOTFILES
+rsync -avh --delete --progress $HOME/.??* $USB_DRIVE_DIR/OLDHOMEDIRDOTFILES # dotfiles
 
 echo "iTHERE ARE DOT FILES IN THIS FOLDER" > $USB_DRIVE_DIR/OLDHOMEDIRDOTFILES/DOTFILES_IN_THIS_FOLDER.txt
 echo "Listing folder contents:"
