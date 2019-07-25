b0b24fabe860558a57402ded461a8dcaf4d4bbb7
Mark Smith <markjgsmith@gmail.com>
2019-07-26 00:27:36 +0700

    Remove webstorm configuration

--- bin/install/gui-configuration.sh
+++ bin/install/gui-configuration.sh
@@ -8,12 +8,3 @@
 # Homebrew packages
 echo "Configuring installed homebrew cask packages..."
 
-echo "Configuring webstorm..."
-version=`brew cask info webstorm | grep webstorm\: | awk '{split($0,a," "); print a[2]}'`
-echo "Webstorm version: $version"
-dir=~/Library/Preferences/WebStorm$version/colors
-base="https://raw.githubusercontent.com/jkaving/intellij-colors-solarized/master"
-echo "Downloading solarized dark colorscheme"
-wget $base/Solarized%20Dark.icls -P $dir
-echo "Downloading solarized light colorscheme"
-wget $base/Solarized%20Light.icls -P $dir
