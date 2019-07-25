1de2f773883eef672ba9697dcf04c8b76dde6021
Mark Smith <markjgsmith@gmail.com>
2019-07-26 00:30:47 +0700

    Remove unused modules beets and haxor-news

--- bin/install/python.sh
+++ bin/install/python.sh
@@ -21,8 +21,6 @@ echo "Current python: `which python`"
 
 echo "Installing python packages..."
 pip2 install --user neovim
-pip install beets
-pip install haxor-news
 
 if [ -x $CONFIGS_DIR/python_local.sh ]; then
   $CONFIGS_DIR/python_local.sh			
