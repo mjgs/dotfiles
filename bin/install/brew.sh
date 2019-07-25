dcf6a4dabfc663c677fa42cdbc2a8f3098ed7f44
Mark Smith <marksmith@Marks-MacBook-Pro.local>
2019-07-26 00:25:51 +0700

    Update curl command spacing

--- bin/install/brew.sh
+++ bin/install/brew.sh
@@ -10,7 +10,7 @@ fi
 if [ ! -x /usr/local/bin/brew ]; then
   echo "Installing homebrew..."
   ruby -e "$(curl \
-    -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+       -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 echo "Installing homebrew packages..."
