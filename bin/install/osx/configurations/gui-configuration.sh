b68751f0ae7bb82b1cb4b71dd4859164e44c6433
Mark Smith <markjgsmith@gmail.com>
2019-07-28 17:59:10 +0700

    Update to use variables for vscode paths

--- bin/install/osx/configurations/gui-configuration.sh
+++ bin/install/osx/configurations/gui-configuration.sh
@@ -16,6 +16,8 @@ set -e; set -o pipefail
 PFX=${PFX:-==>}
 REPO_DIR=${REPO_DIR:?}
 HOME=${HOME:?}
+VSCODE_DOTFILES_DIR=$REPO_DIR/vscode
+VSCODE_USER_SETTINGS_DIR="$HOME/Library/Application\ Support/Code/User"
 
 #
 # Main
@@ -24,9 +26,9 @@ HOME=${HOME:?}
 function configureVscode() {
   echo "$PFX Configuring vscode user settings"
   
-  ln -sf $DOTFILES_DIR/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-  ln -sf $DOTFILES_DIR/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
-  ln -sfn $DOTFILES_DIR/vscode/snippets $HOME/Library/Application\ Support/Code/User/snippets
+  ln -sf $VSCODE_DOTFILES_DIR/settings.json $VSCODE_USER_SETTINGS_DIR/settings.json
+  ln -sf $VSCODE_DOTFILES_DIR/keybindings.json $VSCODE_USER_SETTINGS_DIR/keybindings.json
+  ln -sfn $VSCODE_DOTFILES_DIR/snippets $VSCODE_USER_SETTINGS_DIR/Code/User/snippets
 }
 
 configureVscode
