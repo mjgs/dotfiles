12eb6f1c39b7d2b09b61c6641ebe10d086ae05d1
Mark <markjgsmith@gmail.com>
2019-08-02 16:36:14 +0700

    Refactor to use functions

--- bin/install/common/configurations/cli/neovim.sh
+++ bin/install/common/configurations/cli/neovim.sh
@@ -8,43 +8,60 @@ HOME=${HOME:?}
 XDG_CONFIG_DIR=$HOME/.config
 NVIM_DIR=$XDG_CONFIG_DIR/nvim
 VIM_DIR=$HOME/.vim
+VIM_PLUGIN_MANAGER_REPO=https://github.com/VundleVim/Vundle.vim.git
+VIM_BUNDLES_DIR=$VIM_DIR/bundle
 
 echo "$PFX Configuring package: neovim"
 
-echo "$PFX Linking nvim to vim configuration..."
+function createNvimConfiguration() {
+  echo "$PFX Creating nvim configuration..."
 
-if [ -e $NVIM_DIR ]; then 
-  echo "$PFX $NVIM_DIR already exists... Skipping."    
-else
-  echo "$PFX Creating $HOME/.vim symlink to $NVIM_DIR"
-  ln -s $HOME/.vim $NVIM_DIR
-fi
+  if [ -e $NVIM_DIR ]; then 
+    echo "$PFX $NVIM_DIR already exists... Skipping."    
+  else
+    echo "$PFX Creating $HOME/.vim symlink to $NVIM_DIR"
+    ln -s $HOME/.vim $NVIM_DIR
+  fi
 
-if [ -e $NVIM_DIR/init.vim ]; then 
-  echo "$PFX $NVIM_DIR/init.vim already exists... Skipping." 
-else
-  echo "$PFX Creating $HOME/.vimrc symlink to $NVIM_DIR/init.vim"
-  ln -s $HOME/.vimrc $NVIM_DIR/init.vim
-fi
+  if [ -e $NVIM_DIR/init.vim ]; then 
+    echo "$PFX $NVIM_DIR/init.vim already exists... Skipping." 
+  else
+    echo "$PFX Creating $HOME/.vimrc symlink to $NVIM_DIR/init.vim"
+    ln -s $HOME/.vimrc $NVIM_DIR/init.vim
+  fi
+}
 
-echo "$PFX Installing vim plugin manager Vundle..."
-git clone https://github.com/VundleVim/Vundle.vim.git $VIM_DIR/bundle/Vundle.vim
+function installAndConfigureVimPluginManager() {
+  echo "$PFX Installing vim plugin manager..."
+  echo "$PFX Cloning repo: $VIM_PLUGIN_MANAGER_REPO"
+  echo "$PFX Target directory: $REPO_LOCAL_DIR"
 
-# Silent install of vim plugins doesn't work
-#echo "$PFX Installing vim plugins to $HOME/.vim/bundle (no output to console, might take a while)..."
-#nvim +PluginInstall +PluginUpdate +qall &>/dev/null
-read -p "Open nvim / vim and install plugins :PluginInstall" enter1
-read -p "Open nvim / vim and update plugins :PluginUpdate" enter2
+  if [ ! -e $REPO_LOCAL_DIR ]; then
+    git clone $VIM_PLUGIN_MANAGER_REPO $REPO_LOCAL_DIR
+  else
+    echo "$PFX Target directory exists, skipping..."
+  fi
 
-echo "$PFX Configuring vim color schemes..."
-rsync -avz $VIM_DIR/bundle/vim-colorschemes/colors $VIM_DIR
+  # Silent install of vim plugins doesn't work
+  #echo "$PFX Installing vim plugins to $HOME/.vim/bundle (no output to console, might take a while)..."
+  #nvim +PluginInstall +PluginUpdate +qall &>/dev/null
+  read -p "Open nvim / vim and install plugins :PluginInstall" enter1
+  read -p "Open nvim / vim and update plugins :PluginUpdate" enter2
+}
 
-echo "$PFX Configuring vim web indent..."
-rsync -avz $VIM_DIR/bundle/vim-web-indent/indent $VIM_DIR
+function configureVimColorScheme() {
+  echo "$PFX Configuring vim color schemes..."
+  rsync -avz $VIM_DIR/bundle/vim-colorschemes/colors $VIM_DIR
+}
 
-echo "$PFX When install.sh completes..."
-echo "$PFX Make sure all nvim plugins got installed, open nvim and run:"
-echo ":PluginInstall"
-echo ":PluginUpdate"
+function configureVimWebIndent() {
+  echo "$PFX Configuring vim web indent..."
+  rsync -avz $VIM_DIR/bundle/vim-web-indent/indent $VIM_DIR
+}
+
+createNvimConfiguration
+installAndConfigureVimPluginManager
+configureVimColorScheme
+configureVimWebIndent
 
 exit 0
